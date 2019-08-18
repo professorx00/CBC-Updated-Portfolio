4aad97a9bff14ac1cd5a1d3e242e6ccd147e40e0
Dave Ward <professorx00@gmail.com>
2019-08-18 17:48:14 -0400

    All pages Created. Format is working

---
+++ scripts/portscript.js
@@ -0,0 +1,33 @@
+const left = $(".display-left");
+const right = $(".display-right");
+const slides = $(".slideImg");
+const title = $("#ProgramName");
+
+let slide = 1;
+showDivs(slide);
+
+function plusDivs(n) {
+  showDivs(slide += n);
+}
+
+function showDivs(n) {
+    if(n>slides.length){
+        slide = 1;
+    }
+    if(n<1){
+        slide = slides.length
+    }
+    for(let i = 0; i<slides.length;i++){
+        console.log(slides[i])
+        $(slides[i]).addClass("hide")
+    }
+    $(slides[slide-1]).removeClass("hide")
+    title.text($(slides[slide-1]).attr("data-program"))
+}
+
+left.on("click", ()=>{
+    plusDivs(-1);
+});
+right.on("click", ()=>{
+    plusDivs(1);
+});
